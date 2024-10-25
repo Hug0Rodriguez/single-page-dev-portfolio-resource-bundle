d287e00ec97051a3fa0b3f54ca9be9fe24725fda
Hugo <76200652+Hug0Rodriguez@users.noreply.github.com>
2024-10-25 14:17:34 -0400

    Finished Testimonial Section

--- starter-template/src/sections/About.tsx
+++ starter-template/src/sections/About.tsx
@@ -1,3 +1,23 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from '@/assets/icons/star.svg';
+import bookImage from "@/assets/images/book-cover.png"
+import Image from "next/image";
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return <div className="pb-96">
+    <SectionHeader
+    eyebrow="About Me"
+    title="A Gimplse Into My Word"
+    description="Learn more about who I am, what I do, and what inspires me"
+    />
+    <div>
+      <Card>
+        <StarIcon/>
+        <h3>My Reads</h3>
+        <p>Explore the books shaping my perspectives.</p>
+        <Image src={bookImage} alt="Book Cover" />
+      </Card>
+    </div>
+  </div>;
 };
